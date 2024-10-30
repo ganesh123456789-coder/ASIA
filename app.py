1394d4a61cfb08c2dafa0ba303a41aae734a0ad7
ganesh123456789-coder <malgi47.ganesh@gmail.com>
2024-10-30 23:12:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return 'Hello World!'
+def home():
+    return 'Hello, Flask!'
 
 
 if __name__ ==  '__main__':
